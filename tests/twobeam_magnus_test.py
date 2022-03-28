7f05024e532aca4365a6be28e44ccb31bf6c165a
gurhar1133 <ghns1133@gmail.com>
2022-03-28 19:48:04 +0000

    Fix #1: using rsmath.const instead of rsbeams/rsphysics/const

--- tests/twobeam_magnus_test.py
+++ tests/twobeam_magnus_test.py
@@ -1,13 +1,13 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import rsmath.const as rsc
 from rsfriction.rsensemble import Rs2BeamMagnus
+import rsmath.const
 import scipy
 
 def test_twobeam_magnus():
 
     # specify physical properties of the beam
-    mass_ev = scipy.constants.m_e * rsc.C_SQ / scipy.constants.e  # [eV] mass of an electron
+    mass_ev = scipy.constants.m_e * rsmath.const.C_SQ / scipy.constants.e  # [eV] mass of an electron
     num_elec = 1000
     max_rms_fac = 4.9
     distrib_type = 'gaussian'
@@ -24,7 +24,7 @@ def test_twobeam_magnus():
     # specify physical properties of the ion(s)
     charge_ion_c = scipy.constants.e
     mass_ion_kg = scipy.constants.m_p   # [kg] mass of a proton
-    mass_ion_ev = mass_ion_kg * rsc.KG_to_EV
+    mass_ion_ev = mass_ion_kg * rsmath.const.KG_to_EV
 
     my_2beam = Rs2BeamMagnus.Rs2BeamMagnus(num_elec, charge_ebunch_c, distrib_type, max_rms_fac, \
                                            x_rms_m, vx_rms_mks, y_rms_m, vy_rms_mks, z_rms_m, vz_rms_mks, \
